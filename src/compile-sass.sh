3ecac94142961c21ae471acd1033edd29737de9b
Vidhu Kant Sharma <vidhukant@protonmail.ch>
2022-05-28 14:47:22 +0530

    removed blog code

--- src/compile-sass.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-cmd="sass $@"
-
-$cmd web/styles/styles.scss:../blog/css/styles.css &
-$cmd web/styles/home.scss:../blog/css/home.css &
-$cmd web/styles/post/posts.scss:../blog/css/posts.css &
-$cmd web/styles/post/post.scss:../blog/css/post.css &
-

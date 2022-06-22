f01d2bffd813885f66ae16362deb6fa4be622d94
Vidhu Kant Sharma <vidhukant@vidhukant.xyz>
2022-06-22 14:32:57 +0530

    pushing script to compile sass

---
+++ public/compile-sass.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+mkdir -p css/projects
+
+cmd="sass $@"
+
+$cmd scss/styles.scss:css/styles.css
+$cmd scss/link-tree.scss:css/link-tree.css
+$cmd scss/font-faces.scss:css/font-faces.css
+$cmd scss/index.scss:css/index.css
+
+$cmd scss/projects/styles.scss:css/projects/styles.css
